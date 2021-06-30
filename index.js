8d069725870d49d5b3af181373927656d53848f5
cjtrahey <trahey.03@gmail.com>
2021-06-30 18:36:36 -0400

    added files, added base work

---
+++ index.js
@@ -0,0 +1,48 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = (answers) => {
+
+
+// TODO: Create an array of questions for user input
+const questions = ['intro', 'product', 'concept', 'technologies', 'documentation'];
+}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) 
+    .prompt ([
+        {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+         type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    ])
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
