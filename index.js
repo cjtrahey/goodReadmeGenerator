7d4da057b5b5648bb67f479a1bc2db4c75dd5336
Christopher-John Trahey <81052110+cjtrahey@users.noreply.github.com>
2021-07-03 00:57:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,81 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
 
-const generateMarkdown = (answers) => {
-
-
-// TODO: Create an array of questions for user input
-const questions = ['intro', 'product', 'concept', 'technologies', 'documentation'];
-}
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) 
-    .prompt ([
+//Prompt the user questions to populate the README.md
+function promptUser(){
+    return inquirer.prompt([
         {
-        type: '',
-        name: '',
-        message: '',
-    },
-    {
-         type: '',
-        name: '',
-        message: '',
-    },
-    {
-        type: '',
-        name: '',
-        message: '',
-    },
-    {
-        type: '',
-        name: '',
-        message: '',
-    },
-    {
-        type: '',
-        name: '',
-        message: '',
-    },
-    ])
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "How was this project installed? (if any)",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project being used for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "MIT",
+                "GNU",
+                "Mozilla",
+            ]
+        },
+        {
+            type: "input",
+            name: "contribute",
+            message: "Did anyone contribute to this project? List them here."
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
 
-// TODO: Create a function to initialize app
-function init() {
 
-}
+  function init() {
+     {
+        // asks user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateMarkdown(answers);
 
-// Function call to initialize app
-init();
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('README.md file successfully created!');
+     }
+  }
+  
+  // initializes application
+  init();  
