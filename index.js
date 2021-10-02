bc9600c192d4d37b373d4c72af416e9034d7e088
Christopher-John Trahey <81052110+cjtrahey@users.noreply.github.com>
2021-10-01 22:20:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,181 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown")
+let inquirer = require('inquirer');
+let fs = require('fs');
 
-//Prompt the user questions to populate the README.md
-function promptUser(){
-    return inquirer.prompt([
+inquirer
+    .prompt([
         {
             type: "input",
-            name: "projectTitle",
-            message: "What is the project title?",
+            message: "What is the title of your project?",
+            name: "title"
         },
         {
             type: "input",
-            name: "description",
-            message: "Write a brief description of your project: "
+            message: "Enter a description for your project:",
+            name: "description"
         },
         {
             type: "input",
-            name: "installation",
-            message: "How was this project installed? (if any)",
+            message: "Enter installation insructions as a comma separated list:",
+            name: "install"
         },
         {
             type: "input",
-            name: "usage",
-            message: "What is this project being used for?"
+            message: "Enter usage information for your project:",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Enter contribution guidelines for your project:",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Enter test information for your project:",
+            name: "testing"
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub Username:",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "Enter your email address:",
+            name: "email"
         },
         {
             type: "list",
+            message: "Which license does this project fall under?",
             name: "license",
-            message: "Chose the appropriate license for this project: ",
             choices: [
-                "Apache",
-                "MIT",
-                "GNU",
-                "Mozilla",
+                "MIT License",
+                "Code Project Open License (CPOL)",
+                "Common Development and Distribution License (CDDL)",
+                "Microsoft Public License (Ms-PL)",
+                "Mozilla Public License 1.1 (MPL 1.1)",
+                "Common Public License Version 1.0 (CPL)",
+                "Eclipse Public License 1.0",
+                "Apache License, Version 2.0"
             ]
-        },
-        {
-            type: "input",
-            name: "contribute",
-            message: "Did anyone contribute to this project? List them here."
-        },
-        {
-            type: "input",
-            name: "tests",
-            message: "Is there a test included?"
-        },
-        {
-            type: "input",
-            name: "questions",
-            message: "What do I do if I have an issue? "
-        },
-        {
-            type: "input",
-            name: "username",
-            message: "Please enter your GitHub username: "
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter your email: "
         }
-    ]);
-} 
-
-
-  function init() {
-     {
-        // asks user questions and generate responses
-        const answers = await promptUser();
-        const generateContent = generateMarkdown(answers);
-
-        // Write new README.md to dist directory
-        await writeFileAsync('./dist/README.md', generateContent);
-        console.log('README.md file successfully created!');
-     }
-  }
-  
-  // initializes application
-  init();  
+    ])
+    .then((res) => {
+        console.log("Creating README file...");
+        createREADMEFile(res);
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    
+    
+function createREADMEFile(input) {
+    let readmeTitle;
+    let readmeDescription;
+    const descriptionHead = "## Description";
+    let tableOfContents;
+    const tocHead = "## Table of Contents";
+    let installArr;
+    const installHead = "## Installation";
+    let readmeUsage;
+    const usageHead = "## Usage";
+    let readmeContribution;
+    const contributionHead = "## Contribution";
+    let readmeTest;
+    const testingHead = "## Tests";
+    let readmeLicence = input.license;
+    const licenseHead = "## License";
+    let readmeQuestions;
+    const questionsHead = "## Questions";
+    let completeREADME = [];
+    
+    // Adding Title
+    if (input.title == '') {
+        readmeTitle = '# TITLE';
+    } else {
+        readmeTitle = `# ${input.title}`;
+    }
+    completeREADME.push(readmeTitle);
+    
+    
+    //Add in license badge here!!
+    let badge = `![](https://img.shields.io/badge/license-${readmeLicence.replace(/ /g, "%20")}-blue?style=flat-square)`;
+    completeREADME.push(badge);
+    
+    
+    // Adding description
+    if (input.description == '') {
+        readmeDescription = `${descriptionHead}\n Enter project description here.`;
+    } else {
+        readmeDescription = `${descriptionHead}\n${input.description}`;
+    }
+    completeREADME.push(readmeDescription);
+    
+    
+    //Adding Table of Contents
+    tableOfContents = `${tocHead}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contribution](#contribution)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)\n`;
+    completeREADME.push(tableOfContents);
+    
+    
+    //Adding installation instructions
+    completeREADME.push(`${installHead}`);
+    
+    installArr = input.install.split(',').map(item => {
+        return `${item.trim()}`;
+    });
+    
+    for (var i = 0; i < installArr.length; i++) {
+        completeREADME.push(`${i + 1}. ${installArr[i]}`);
+    }
+    
+    
+    //Adding Usage
+    if (input.usage == '') {
+        readmeUsage = `\n${usageHead}\n Enter project usage here.`;
+    } else {
+        readmeUsage = `\n${usageHead}\n${input.usage}`;
+    }
+    completeREADME.push(readmeUsage);
+    
+    
+    //Adding Contributing
+    if (input.contribution == '') {
+        readmeContribution = `\n${contributionHead}\n Enter project contriburtion information here.`;
+    } else {
+        readmeContribution = `\n${contributionHead}\n${input.contribution}`;
+    }
+    completeREADME.push(readmeContribution);
+    
+    
+    //Adding Tests
+    if (input.testing == '') {
+        readmeTest = `\n${testingHead}\n Enter project testing information here.`;
+    } else {
+        readmeTest = `\n${testingHead}\n${input.testing}`;
+    }
+    completeREADME.push(readmeTest);
+    
+    
+    //License info
+    readmeLicence = `\n${licenseHead}\nThis project is convered under the ${input.license}.`;
+    completeREADME.push(readmeLicence);
+    
+    
+    //Questions section with github link
+    readmeQuestions = `\n${questionsHead}\nFor questions about this project, please see my GitHub at [${input.github}](https://github.com/${input.github}), or reach out by email at ${input.email}.`;
+    completeREADME.push(readmeQuestions);
+    
+    
+    //Joining the created README Array with a newline separator
+    const README = completeREADME.join('\n');
+        
+    
+    //Creating the README
+    fs.writeFile("./example/README-example.md", README, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("README file successfully created!");
+        }
+    });
+}
